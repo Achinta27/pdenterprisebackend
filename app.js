214b8199db8f94b91d55dcc85ba0bd3f8a117676
fireFistAniket <sahaaniket720@gmail.com>
2025-08-13 23:58:59 +0530

    fix

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ try {
 }
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
 app.use(
   fileUpload({
